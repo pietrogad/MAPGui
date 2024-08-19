3781b970f0b3b568e9b760443c4adfc84d910c7f
pietrogad <gadapie.03@gmail.com>
2024-08-19 17:17:15 +0200

    fix

--- src/main/java/com/example/clientmapgui/Main.java
+++ src/main/java/com/example/clientmapgui/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CaricaDaDatabase.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("ConnessioneServer.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setResizable(false);
         stage.setTitle("Client");
